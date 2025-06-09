78651f29d69ce1859e8b9c479557e097ef538388
kishore-gea <Prudhivi.Kishore@geappliances.com>
2025-06-09 12:51:05 +0530

    Updates

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"1425e5e9ec5eeb4f225c401d8db69b860e0fde
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3071116474"
+    serviceWorkerVersion: "2252159221"
   }
 });
